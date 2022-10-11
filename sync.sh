8c5bdd5ccc2adb3536040c3898f59b3f8d89cb9b
mate <115540974+mate-4404@users.noreply.github.com>
2022-10-11 15:33:22 +0530

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,12 @@
+
+git remote add <name> <url>
+#create a new connection to a remote repo.
+
+git fetch 
+# get all the changes from  the origin (no merge)
+
+git pull
+# get all the latest changes from the origin and merge
+
+git push
+# used to upload your local repository changes to te origin remote repo
