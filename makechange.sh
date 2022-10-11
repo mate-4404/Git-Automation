577359ee02d0332efa4a4000dfbd59671aaf6a94
mate <115540974+mate-4404@users.noreply.github.com>
2022-10-11 15:31:50 +0530

    Create makechange.sh

---
+++ makechange.sh
@@ -0,0 +1,11 @@
+git status  
+#lists new or modified files not yet committed
+
+git diff #{file...}
+#list down changes and conflicts
+
+git reset #{file.....}
+#unstages file,keeping the file changes
+
+git commit -m "[descriptive message]"
+#commit all the stages to virsioned history
