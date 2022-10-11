1269824d362162821e2607f8b031f5d4474c297f
mate <115540974+mate-4404@users.noreply.github.com>
2022-10-11 15:32:31 +0530

    Create merge.sh

---
+++ merge.sh
@@ -0,0 +1,13 @@
+# git branching & merging 
+
+git branch
+#will list your branches
+
+git checkout
+#switch another branch and check it out into your working directory
+
+git merge 
+# will merge the specified branch's history into the current branch
+
+git log
+#show all the commits in the current branch history
